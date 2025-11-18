0c28c0e23948cad3bc3b0643c25abbfc3a8bc67d
Matheus Henrique <matheuseevee@gmail.com>
2025-11-18 12:37:39 -0300

    Create financeiro.sql

---
+++ db/financeiro.sql
@@ -0,0 +1,114 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           8.4.3 - MySQL Community Server - GPL
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.8.0.6908
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para financeiro
+CREATE DATABASE IF NOT EXISTS `financeiro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `financeiro`;
+
+-- Copiando estrutura para tabela financeiro.categorias
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `usuario_id` int unsigned NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `tipo_padrao` enum('entrada','saida') NOT NULL,
+  `criado_em` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_categorias_usuario` (`usuario_id`),
+  CONSTRAINT `fk_categorias_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financeiro.contas
+CREATE TABLE IF NOT EXISTS `contas` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `usuario_id` int unsigned NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `saldo_inicial` decimal(12,2) NOT NULL DEFAULT '0.00',
+  `criado_em` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_contas_usuario` (`usuario_id`),
+  CONSTRAINT `fk_contas_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financeiro.formas_pagamento
+CREATE TABLE IF NOT EXISTS `formas_pagamento` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `usuario_id` int unsigned NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `criado_em` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `fk_fp_usuario` (`usuario_id`),
+  CONSTRAINT `fk_fp_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financeiro.lancamentos
+CREATE TABLE IF NOT EXISTS `lancamentos` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `usuario_id` int unsigned NOT NULL,
+  `conta_id` int unsigned NOT NULL,
+  `categoria_id` int unsigned DEFAULT NULL,
+  `forma_pagamento_id` int unsigned DEFAULT NULL,
+  `tipo` enum('entrada','saida') NOT NULL,
+  `valor` decimal(12,2) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `data_movimento` datetime NOT NULL,
+  `saldo_apos` decimal(12,2) DEFAULT NULL,
+  `observacoes` text,
+  `criado_em` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `apagado_em` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_lanc_fp` (`forma_pagamento_id`),
+  KEY `idx_lanc_usuario_data` (`usuario_id`,`data_movimento`),
+  KEY `idx_lanc_conta_data` (`conta_id`,`data_movimento`),
+  KEY `idx_lanc_categoria` (`categoria_id`),
+  KEY `idx_lanc_tipo` (`tipo`),
+  CONSTRAINT `fk_lanc_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `fk_lanc_conta` FOREIGN KEY (`conta_id`) REFERENCES `contas` (`id`) ON DELETE RESTRICT,
+  CONSTRAINT `fk_lanc_fp` FOREIGN KEY (`forma_pagamento_id`) REFERENCES `formas_pagamento` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `fk_lanc_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela financeiro.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `ativo` tinyint(1) DEFAULT '1',
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `senha_hash` varchar(255) DEFAULT NULL,
+  `api_token` char(36) DEFAULT NULL,
+  `firebase_uid` varchar(128) DEFAULT NULL,
+  `criado_em` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `idx_usuarios_api_token` (`api_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
